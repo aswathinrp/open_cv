5f25a8a0a2021aa37848b7fd142d6a4e739fd385
aswathinrp <90610747+aswathinrp@users.noreply.github.com>
2022-09-29 13:06:35 +0530

    feature branch created

--- sthreshold.py
+++ sthreshold.py
@@ -1,6 +1,6 @@
 import cv2
 
-img=cv2.imread('',0)
+img=cv2.imread('g.jpg',0)
 _,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 _,th2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
 _,th3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
